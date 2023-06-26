a334d6d32fa29c480d5329c16608bbd8a0fabbcd
Scolopendria <probablyEukaryotic@gmail.com>
2023-06-25 20:05:14 -0700

    speed testing

--- main.cpp
+++ main.cpp
@@ -2,27 +2,55 @@
 #include <array>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
-void solver(std::array<int, 64> board, std::array<int, 64> legalPossibilities, int moveNumber, int square);
-bool update(std::array<int, 64> &board, std::array<int, 64> &legalPossibilities, int moveNumber, int square);
-void initialize(std::array<int, 64> &board, std::array<int, 64> &legalPossibilities);
-void printBoard(std::array<int, 64> board);
+// Starting square must be less than 64 
+// or program will crash on the first line of update.
+// No safety net
+#define STARTING_SQUARE 0
+// Calculating time defines the amount of time (in µs) allocated to find solutions
+#define CALCULTING_TIME 1
+
+void solver(
+    std::array<int, 64>  board, std::array<int, 64>  legalPossibilities,
+    const int &moveNumber, const int& square, int& solutionCount,
+    const std::chrono::steady_clock::time_point &startTime
+);
+bool update(
+    std::array<int, 64> &board, std::array<int, 64> &legalPossibilities,
+    const int &moveNumber, const int& square, int& solutionCount,
+    const std::chrono::steady_clock::time_point &startTime
+);
+void printBoard(const std::array<int, 64>& board);
 
 int main() {
-    std::cout << "Hello world!" << std::endl;
-    std::array<int, 64> board, legalPossibilities;
-    initialize(board, legalPossibilities);
-    update(board, legalPossibilities, 1, 35);
-    solver(board, legalPossibilities, 1, 35);
+    std::array<int, 64> board,
+        legalPossibilities = {
+            2, 3, 4, 4, 4, 4, 3, 2,
+            3, 4, 6, 6, 6, 6, 4, 3,
+            4, 6, 8, 8, 8, 8, 6, 4,
+            4, 6, 8, 8, 8, 8, 6, 4,
+            4, 6, 8, 8, 8, 8, 6, 4,
+            4, 6, 8, 8, 8, 8, 6, 4,
+            3, 4, 6, 6, 6, 6, 4, 3,
+            2, 3, 4, 4, 4, 4, 3, 2
+        };
+    auto startTime = std::chrono::steady_clock::now();
+    int solutionCount = 0;
+    update(board, legalPossibilities, 1, STARTING_SQUARE, solutionCount, startTime);
+    solver(board, legalPossibilities, 1, STARTING_SQUARE, solutionCount, startTime);
 
     return 0;
 }
 
-void solver(std::array<int, 64> board, std::array<int, 64> legalPossibilities, int moveNumber, int square) {    
+void solver(
+    std::array<int, 64>  board, std::array<int, 64>  legalPossibilities,
+    const int &moveNumber, const int& square, int& solutionCount,
+    const std::chrono::steady_clock::time_point &startTime
+) {
     int x = square % 8, y = square / 8;
     std::array<std::array<int, 2>, 8> indexPair;
     indexPair.fill(std::array<int, 2>{-1, 255});
-    // Change to single dimensional array and pass legalP to sort?
 
     // Fill indexPair with valid index and possibility pairs
     if (x <= 6 && y >= 2) {
@@ -57,7 +85,6 @@ void solver(std::array<int, 64> board, std::array<int, 64> legalPossibilities, i
         indexPair[7] = std::array<int, 2>{square - 17, legalPossibilities[square - 17]};
     }
 
-
     // Order indexes on their possibilites
     std::sort(
         indexPair.begin(),
@@ -70,77 +97,92 @@ void solver(std::array<int, 64> board, std::array<int, 64> legalPossibilities, i
     // Loop throigh the indexPair
     for (auto [index, possibility]: indexPair) {
         if (possibility > 100) continue;
-        auto state = update(board, legalPossibilities, moveNumber+1, index);
-        if (state) {
-            solver(board, legalPossibilities, moveNumber+1, index);
+        auto copyBoard = board;
+        auto copyPossibilities = legalPossibilities;
+        if (update(copyBoard, copyPossibilities, moveNumber+1, index, solutionCount, startTime)) {
+            solver(copyBoard, copyPossibilities, moveNumber+1, index, solutionCount, startTime);
         }
     }
 }
 
-bool update(std::array<int, 64> &board, std::array<int, 64> &legalPossibilities, int moveNumber, int square) {
-    board[square] = moveNumber;
+bool update(
+    std::array<int, 64> &board, std::array<int, 64> &legalPossibilities,
+    const int &moveNumber, const int& square, int& solutionCount,
+    const std::chrono::steady_clock::time_point &startTime
+) { board[square] = moveNumber;
 
     if (moveNumber == 64) {
-        printBoard(board);
-        exit(EXIT_SUCCESS);
+        solutionCount++;
+        auto endTime = std::chrono::steady_clock::now();
+        
+        if (std::chrono::duration_cast<std::chrono::microseconds> (endTime - startTime).count() > CALCULTING_TIME) {
+            std::cout << "Solution " << solutionCount << " took " <<
+                std::chrono::duration_cast<std::chrono::microseconds> (endTime - startTime).count() << "µs\n";
+            printBoard(board);
+            exit(EXIT_SUCCESS);
+        }
+
+        board[square] = 0;
+        return false;
     }
 
-    auto copy = legalPossibilities;
+    auto copyPossibilities = legalPossibilities;
+    
     int x = square % 8, y = square / 8;
     std::vector<int> disconnected;
 
     if (x <= 6 && y >= 2) {
-        copy[square - 15]--;
-        if (copy[square - 15] == 0) {
+        copyPossibilities[square - 15]--;
+        if (copyPossibilities[square - 15] == 0) {
             disconnected.push_back(square - 15);
         }
     }
 
     if (x <= 5 && y >= 1) {
-        copy[square - 6]--;
-        if (copy[square - 6] == 0) {
+        copyPossibilities[square - 6]--;
+        if (copyPossibilities[square - 6] == 0) {
             disconnected.push_back(square - 6);
         }
     }
 
     if (x <= 5 && y <= 6) {
-        copy[square + 10]--;
-        if (copy[square + 10] == 0) {
+        copyPossibilities[square + 10]--;
+        if (copyPossibilities[square + 10] == 0) {
             disconnected.push_back(square + 10);
         }
     }
 
     if (x <= 6 && y <= 5) {
-        copy[square + 17]--;
-        if (copy[square + 17] == 0) {
+        copyPossibilities[square + 17]--;
+        if (copyPossibilities[square + 17] == 0) {
             disconnected.push_back(square + 17);
         }
     }
 
     if (x >= 1 && y <= 5) {
-        copy[square  + 15]--;
-        if (copy[square + 15] == 0) {
+        copyPossibilities[square  + 15]--;
+        if (copyPossibilities[square + 15] == 0) {
             disconnected.push_back(square + 15);
         }
     }
 
     if (x >= 2 && y <= 6) {
-        copy[square + 6]--;
-        if (copy[square + 6] == 0) {
+        copyPossibilities[square + 6]--;
+        if (copyPossibilities[square + 6] == 0) {
             disconnected.push_back(square + 6);
         }
     }
     
     if (x >= 2 && y >= 1) {
-        copy[square - 10]--;
-        if (copy[square - 10] == 0) {
+        copyPossibilities[square - 10]--;
+        if (copyPossibilities[square - 10] == 0) {
             disconnected.push_back(square - 10);
         }
     }
 
     if (x >= 1 && y >= 2) {
-        copy[square - 17]--;
-        if (copy[square - 17] == 0) {
+        copyPossibilities[square - 17]--;
+        if (copyPossibilities[square - 17] == 0) {
             disconnected.push_back(square - 17);
         }
     }
@@ -149,80 +191,21 @@ bool update(std::array<int, 64> &board, std::array<int, 64> &legalPossibilities,
         board[square] = 0;
         return false;
     } else if (disconnected.size() == 1) {
-        auto state = update(board, copy, moveNumber+1, disconnected.front());
-        if (state) {
-            solver(board, copy, moveNumber+1, disconnected.front());
+        copyPossibilities[square] = 255;
+        if (update(board, copyPossibilities, moveNumber+1, disconnected.front(), solutionCount, startTime)) {
+            solver(board, copyPossibilities, moveNumber+1, disconnected.front(), solutionCount, startTime);
         }
 
         board[square] = 0;
         return false;
     }
 
-    legalPossibilities = copy;
+    legalPossibilities = copyPossibilities;
     legalPossibilities[square] = 255;
     return true;
 }
 
-void initialize(std::array<int, 64> &board, std::array<int, 64> &legalPossibilities) {
-    // fill board with 0s
-    board.fill(0);
-
-    // initialize legalPossibilities
-    legalPossibilities.fill(8);
-
-    for (int i = 2; i < 6; i++) {
-        legalPossibilities[i] = 4;
-    }
-
-    for (int i = 10; i < 14; i++) {
-        legalPossibilities[i] = 6;
-    }
-
-    for (int i = 50; i < 54; i++) {
-        legalPossibilities[i] = 6;
-    }
-
-    for (int i = 58; i < 62; i++) {
-        legalPossibilities[i] = 4;
-    }
-
-    for (int i = 16; i < 41; i+=8) {
-        legalPossibilities[i] = 4;
-    }
-
-    for (int i = 17; i < 42; i+=8) {
-        legalPossibilities[i] = 6;
-    }
-
-    for (int i = 22; i < 47; i+=8) {
-        legalPossibilities[i] = 6;
-    }
-
-    for (int i = 23; i < 58; i+=8) {
-        legalPossibilities[i] = 4;
-    }
-
-    legalPossibilities[ 9] = 4;
-    legalPossibilities[14] = 4;
-    legalPossibilities[49] = 4;
-    legalPossibilities[54] = 4;
-
-    legalPossibilities[ 0] = 2;
-    legalPossibilities[ 7] = 2;
-    legalPossibilities[56] = 2;
-    legalPossibilities[63] = 2;
-
-    legalPossibilities[ 1] = 3;
-    legalPossibilities[ 6] = 3;
-    legalPossibilities[ 8] = 3;
-    legalPossibilities[15] = 3;
-    legalPossibilities[48] = 3;
-    legalPossibilities[55] = 3;
-    legalPossibilities[57] = 3;
-    legalPossibilities[62] = 3;
-}
-
-void printBoard(std::array<int, 64> board) {
+void printBoard(const std::array<int, 64>& board) {
     for (int i = 0; i < 64; i++) {
         if (i % 8 == 0) {
             std::cout << "\n";
@@ -233,4 +216,6 @@ void printBoard(std::array<int, 64> board) {
         }
         std::cout << board[i] << " ";
     }
+
+    std::cout << "\n\n\n";
 }
